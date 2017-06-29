064567676e5c18adb964cbb9d0ddf9cf9dde8448
Debbie Milburn <debbiemilburn2@gmail.com>
2017-06-29 09:45:24 -0400

    Move things into appropriate files

--- runner.rb
+++ runner.rb
@@ -1,10 +1,10 @@
 require 'csv'
-require_relative 'shmikea'
-require_relative 'furniture'
+require_relative 'model/shmikea'
+require_relative 'model/furniture'
 require_relative 'furniture_loader'
 
 
-shmikea = Shmikea.new('shmikea.csv')
+shmikea = Shmikea.new('db/shmikea.csv')
 puts "Here's the catalogue!"
 shmikea.furniture.each do |furniture|
   puts furniture
